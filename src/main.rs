fdadb1bd4c1a40656d778b91ee06df87e3543e93
Madeline Zelenskyy <madelinezelenskyy@gmail.com>
2025-02-18 22:09:00 -0800

    Exit code

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,12 @@ fn main() {
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
-
+        
+        if(input != "exit") {
         println!("{}: command not found", input.trim());
         input.clear();
+        } else {
+            return 0;
+        }
     }
 }
