74f9396b94ac37fc49ff5994e184759e86445c86
Madeline Zelenskyy <madelinezelenskyy@gmail.com>
2025-02-18 21:59:08 -0800

    Initial shell script

--- src/main.rs
+++ src/main.rs
@@ -11,5 +11,6 @@ fn main() {
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
 
-    println!("{}: Command not found.", input.trim());
+    println!("{}: command not found", input.trim());
+    
 }
